dd934b746fe13dca2e057da428f4a55923518917
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-08-02 13:54:07 -0600

    Started build

--- Index.js
+++ Index.js
@@ -3,3 +3,16 @@ const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+class Boundary {
+    constructor({ position }, ) {
+        this.position = position
+        this.width = 40
+        this.height = 40
+    }
+
+    draw() {
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+}
